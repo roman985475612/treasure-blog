89a0ea8f987f3f11b353c8c7c69facefdc27ee7e
Roman <roman985475612@gmail.com>
2020-09-19 14:15:37 +0300

    Chapter 8 completed

--- config/web.php
+++ config/web.php
@@ -11,6 +11,9 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+//    'layout' => 'test',
+    'language' => 'ru',
+    'defaultRoute' => 'test',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
