8518324de79839adead066d17f320bb20f0c72fc
Roman <roman985475612@gmail.com>
2020-10-26 13:07:46 +0300

    Chapter 12/RM complete

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,6 +64,16 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionSingle()
+    {
+        return $this->render('single');
+    }
+
+    public function actionCategory()
+    {
+        return $this->render('category');
+    }
+
     /**
      * Login action.
      *
